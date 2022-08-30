68560299d4f0e8c86bcadfa270c0c87adcf8a583
zhangfan <cyouho@hotmail.co.jp>
2022-08-30 08:25:08 +0800

    add func commit

--- config/serversname.php
+++ config/serversname.php
@@ -1,5 +1,15 @@
 <?php
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Servers Name
+    |--------------------------------------------------------------------------
+    |
+    | This array shows the default servers name which user can be used.
+    |
+    */
+
     'user_servers_name' => [
         'Warehouse Management Server' => '家用货架管理服务',
     ],
